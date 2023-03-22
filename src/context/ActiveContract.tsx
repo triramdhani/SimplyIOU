dae872725d9ca745d96ce3b29979fa5d89328479
Tri Ramdhani <triaresraamdhani@gmail.com>
2023-03-22 15:36:26 +0700

    feat(\): add zustand package

---
+++ src/context/ActiveContract.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react'
+
+interface IActiveContract {
+  children: ReactNode
+}
+
+const ActiveContract = ({children}:IActiveContract) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ActiveContract
