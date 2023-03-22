0b4bad7f3d645506e602e15f45862c4dd6e4deb8
Tri Ramdhani <triaresraamdhani@gmail.com>
2023-03-22 16:14:16 +0700

    feat: get last contract id when web first render

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
 import PrimaryLayout from '@/components/layouts/primary/PrimaryLayout'
+import {  useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { NextPageWithLayout } from './page'
 
 const Home: NextPageWithLayout = () => {
+  const [lastContractId, setLastContractId] = useState('')
+  const Router = useRouter()
+  useEffect(() => {
+    const value = localStorage.getItem("lastContractId") 
+    if (typeof value === 'string') {
+        const parseItem = JSON.parse(value) // ok
+        setLastContractId(parseItem)
+    }
+  }, []);
+  
+  if(lastContractId !== '' ){
+    setTimeout(() => {
+      Router.push(`/${lastContractId}`)
+    }, 1000);
+    return (
+      <div>Loading.....</div>
+    )
+  }
   return (
     <>
       <section className='flex min-w-full justify-center '>
