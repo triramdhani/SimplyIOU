d959da1df70efccb53fca53647e549be35f1b593
Tri Ramdhani <triaresraamdhani@gmail.com>
2023-03-29 17:12:56 +0700

    refactor: refactor newcontract page

--- src/pages/newContract.tsx
+++ src/pages/newContract.tsx
@@ -3,25 +3,30 @@ import CardSplitBill from '@/components/Ui/CardSplitBill'
 import { useState } from 'react'
 import { NextPageWithLayout } from './page'
 
+
+const contractList = {
+  SPLITBILL : 'splitBill',
+  IOU : 'IOU'
+}
+let defaultValue:String = contractList.SPLITBILL || contractList.IOU
+
 const NEW: NextPageWithLayout = () => {
-    const [isSplitBill, setIsSplitBill] = useState(true)
+    const [contractType, setContractType] = useState(defaultValue)
   return (
     <>
       <section className='w-10/12 md:w-9/12 m-auto '>
         <p className='text-2xl px-2 text-center'>create new contract</p>
        
         <div className='grid grid-flow-col auto-cols-fr my-4 cursor-pointer'>
-            <div className={`${isSplitBill ? "bg-purple-500 text-white rounded-md" : "text-gray-400 hover:bg-gray-50 hover:text-gray-500"}`}>
-                <p onClick={()=>setIsSplitBill(true)} className="p-3 w-full inline-flex justify-center items-center text-center text-base font-semibold">Split Bill</p>
+            <div className={`${contractType === contractList.SPLITBILL ? "bg-purple-500 text-white rounded-md" : "text-gray-400 hover:bg-gray-50 hover:text-gray-500"}`}>
+                <p onClick={()=>setContractType(contractList.SPLITBILL)} className="p-3 w-full inline-flex justify-center items-center text-center text-base font-semibold">Split Bill</p>
             </div>
-            <div className={`${!isSplitBill ? "bg-purple-500 text-white rounded-md" : "text-gray-400 hover:bg-gray-50 hover:text-gray-500"}`}>
-                <p onClick={()=>setIsSplitBill(false)} className="p-3 w-full inline-flex justify-center items-center text-center text-base font-semibold">IOU</p>
-            </div>
-            
-            {/* <p onClick={()=>setIsSplitBill(false)}>IOU</p> */}
+            <div className={`${contractType === contractList.IOU ? "bg-purple-500 text-white rounded-md" : "text-gray-400 hover:bg-gray-50 hover:text-gray-500"}`}>
+                <p onClick={()=>setContractType(contractList.IOU)} className="p-3 w-full inline-flex justify-center items-center text-center text-base font-semibold">IOU</p>
+            </div>            
         </div>
         {
-            isSplitBill ? 
+            contractType === contractList.SPLITBILL ? 
             (
                 <CardSplitBill/>
             ):null
